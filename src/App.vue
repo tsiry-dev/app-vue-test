9a87d933d9f31641ad4157992745afc48e2902ac
Tsiry <andriamiarijaonatsiri@gmail.com>
2025-06-14 15:01:34 +0300

    haha

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,7 @@
 
 <template>
    <h1>My app</h1>
+   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil numquam dolore dignissimos dicta ad, repudiandae suscipit nemo maxime consequuntur modi ipsa dolorem ducimus quam minus nisi aperiam exercitationem, architecto a!</p>
 </template>
 
 <style scoped>
